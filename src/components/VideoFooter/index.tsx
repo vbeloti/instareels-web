d6015be71788a1554b72932f185c0c53f41ad434
unknown <viniciusbeloti7@gmail.com>
2020-09-04 11:18:28 -0300

    feat: add react ticker

--- src/components/VideoFooter/index.tsx
+++ src/components/VideoFooter/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Button, Avatar } from "@material-ui/core";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import Ticker from "react-ticker";
 
 import "./styles.css";
 
@@ -10,11 +13,32 @@ interface IVideoFooter {
   shares?: number;
 }
 
-const VideoFooter: React.FC<IVideoFooter> = () => {
+const VideoFooter: React.FC<IVideoFooter> = ({
+  channel,
+  avatarSrc,
+  song,
+  likes,
+  shares,
+}) => {
   return (
-      <div className="videoFooter">
-          
+    <div className="videoFooter">
+      <div className="videoFooter__text">
+        <Avatar src={avatarSrc} />
+        <h3>
+          {channel} • <Button>Follow</Button>
+        </h3>
       </div>
+      <div className="videoFooter__ticker">
+        <MusicNoteIcon className="videoFooter__icon" />
+        <Ticker mode="smooth">
+          {({ index }) => (
+            <>
+              <h1>{song}</h1>
+            </>
+          )}
+        </Ticker>
+      </div>
+    </div>
   );
 };
 
