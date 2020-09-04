ed2f0a378afe45588b9f9a9f36875377381df0c8
unknown <viniciusbeloti7@gmail.com>
2020-09-04 10:04:25 -0300

    feat: add Video

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import "./App.css";
+import logo from './assets/img/logo.png';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="app">
+      <h1>Hello World</h1>
+
+      <div className="app__top">
+        <img src={logo} alt="Instagram Logo" className="app__logo"/>
+        <h1>Reels</h1>
+      </div>
+
+      <div className="app__videos">
+
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Video/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const Video: React.FC = () => {
+  return <div />;
+}
+
+export default Video;
