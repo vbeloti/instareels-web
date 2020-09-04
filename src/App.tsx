abd4fe6d301f2da79999f7b8d9f5bef748a7584e
unknown <viniciusbeloti7@gmail.com>
2020-09-04 11:36:22 -0300

    refactor: change logo app

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,8 @@ import Video from "./components/Video";
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
-
       <div className="app__top">
         <img src={logo} alt="Instagram Logo" className="app__logo" />
-        <h1>Reels</h1>
       </div>
 
       <div className="app__videos">
