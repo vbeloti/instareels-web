668c2a169b35e968205c58d5dd56ff56bf35dbcc
unknown <viniciusbeloti7@gmail.com>
2020-09-04 10:42:03 -0300

    feat: add VideoHeader

---
+++ src/components/VideoHeader/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const VideoHeader: React.FC = () => {
+  return (
+      <div className="videoHeader">
+          <h1>videoHeader</h1>
+      </div>
+  );
+}
+
+export default VideoHeader;
